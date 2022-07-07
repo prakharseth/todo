d80e17260d9cdeb7f5709186f3a172a19908db2c
prakharseth <56907863+prakharseth@users.noreply.github.com>
2022-07-08 00:09:08 +0530

    html tag

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,17 @@ import Head from "next/head";
 import DarkMob from "./DarkMob";
 function IndexPage() {
   return (
-    <div className='font-josef'>
+    <html className='font-josef'>
       <Head>
         <title>PS | TODO APP</title>
         <link rel='icon' href='p.png' />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3575846752993630"
         crossorigin="anonymous"></script>
       </Head>
-      <DarkMob />
-    </div>
+      <body>
+        <DarkMob />
+      </body>
+    </html>
   );
 }
 
