093fbbb1ca1ff6df5947c0beb5d69ff56120ad0d
prakharseth <sethji2003@gmail.com>
2022-07-09 22:26:02 +0530

    head-fix

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,9 @@ function IndexPage() {
       <Head>
         <title>PS | TODO APP</title>
         <link rel='icon' href='p.png' />
-<<<<<<< HEAD
-        <script
-          async
-          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3575846752993630'
-          crossorigin='anonymous'
-        ></script>
       </Head>
-      <div>
-        <DarkMob />
-      </div>
-    </div>
-=======
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3575846752993630"
-        crossorigin="anonymous"></script>
-      </Head>
-      <body>
-        <DarkMob />
-      </body>
+      <DarkMob />
     </html>
->>>>>>> d80e17260d9cdeb7f5709186f3a172a19908db2c
   );
 }
 
